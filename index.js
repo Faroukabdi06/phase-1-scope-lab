776e6796c6d20aabefab7a464ee164c75ba356c5
Farouk Abdi <abdifarouk58@gmail.com>
2025-04-08 00:00:46 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+var customerName = "bob";
+let bestCustomer ;
+const leastFavoriteCustomer = "could be bob"
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+
+}
+// console.log(upperCaseCustomerName())
+
+function setBestCustomer(){
+    window.bestCustomer = "not bob"
+    return bestCustomer;
+
+
+}
+// console.log(setBestCustomer())
+
+function overwriteBestCustomer(){
+    window.bestCustomer = "maybe bob"
+    return bestCustomer;
+
+}
+// console.log(overwriteBestCustomer())
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "it's bob"
+}
+// console.log(changeLeastFavoriteCustomer())
